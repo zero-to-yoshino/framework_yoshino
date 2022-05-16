05be011a60cf4a31ab651ce92b56da040b6d4cad
zero-to-yoshino <k-yoshino@croooober.co.jp>
2022-05-16 10:36:57 +0900

    view1完成

--- app/controllers/HomeController.java
+++ app/controllers/HomeController.java
@@ -18,17 +18,24 @@ public class HomeController extends Controller {
      * <code>GET</code> request with a path of <code>/</code>.
      */
     public Result index() {
-        return ok(views.html.index.render());
+        // リロードすると同じデータが作られるので注意！！
+        // Entry user = new Entry("yoshino", "test", "初testです。");
+        // DB.save(user);
+        // Entry user2 = new Entry("yoshino2", "test2", "test二回目です。");
+        // DB.save(user2);
+        List<Entry> foundEntries = DB.find(Entry.class).findList();
+        // String name = foundUsers.get(1).message.toString();
+        return ok(views.html.index.render(foundEntries));
     }
 
     public Result test(){
         // リロードすると同じデータが作られるので注意！！
-        Entry user = new Entry("yoshino", "test", "初testです。");
-        DB.save(user);
-        Entry user2 = new Entry("yoshino2", "test2", "test二回目です。");
-        DB.save(user2);
-        List<Entry> foundUsers = DB.find(Entry.class).findList();
-        String name = foundUsers.get(1).message.toString();
+        // Entry user = new Entry("yoshino", "test", "初testです。");
+        // DB.save(user);
+        // Entry user2 = new Entry("yoshino2", "test2", "test二回目です。");
+        // DB.save(user2);
+        List<Entry> foundEntries = DB.find(Entry.class).findList();
+        String name = foundEntries.get(1).message.toString();
         // String name = "test";
         return ok(views.html.test.render(name));
     }

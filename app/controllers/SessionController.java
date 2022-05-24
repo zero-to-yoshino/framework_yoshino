bf89d08dcbf8a5da66d409aae65dd15d6b297dc2
zero-to-yoshino <k-yoshino@croooober.co.jp>
2022-05-24 11:56:33 +0900

    手打ちログイン

--- app/controllers/SessionController.java
+++ app/controllers/SessionController.java
@@ -1,5 +1,44 @@
 package controllers;
 
-public class SessionController {
-    
+import models.User;
+import models.Entry;
+import play.mvc.*;
+import play.mvc.Http;
+import play.data.Form;
+import play.data.FormFactory;
+import play.i18n.MessagesApi;
+import io.ebean.DB;
+import javax.inject.Inject;
+import javax.inject.Singleton;
+
+@Singleton
+public class SessionController extends Controller {
+    private final FormFactory formFactory;
+    private MessagesApi messagesApi;
+
+    @Inject
+    public SessionController (FormFactory formFactory, MessagesApi messagesApi) {
+        this.formFactory = formFactory;
+        this.messagesApi = messagesApi;
+    }
+
+    public Result login(Http.Request request) {
+        Form<User> loginForm = formFactory.form(User.class);
+        return ok(views.html.login.render(loginForm, request, messagesApi.preferred(request)));
+    }
+
+    public Result authenticate(Http.Request request) {
+        Form<User> loginForm = formFactory.form(User.class).bindFromRequest(request);
+        if (loginForm.hasErrors()) {
+            // This is the HTTP rendering thread context
+            return badRequest(views.html.login.render(loginForm, request, messagesApi.preferred(request)));
+        } else {
+            User user = loginForm.get();
+            if (user.getEmail().equals("a@b") && user.getPassword().equals("yoshino")) {
+                return Results.redirect(routes.HomeController.index());
+            } else {
+                return Results.redirect(routes.SessionController.login());
+            }
+        }
+    }
 }
--- app/models/User.java
+++ app/models/User.java
@@ -1,5 +1,46 @@
 package models;
 
-public class User {
-    
+import javax.persistence.*;
+// import javax.persistence.Id;
+ 
+import io.ebean.Model;
+// import io.ebean.annotation.*;
+ 
+@Entity
+public class User extends Model{
+    @Id
+    private Long userId;
+
+    private String name;
+    private String email;
+    private String password;
+
+    public Long getUserId(){
+        return userId;
+    }
+    public void setUserId(Long userId){
+        this.userId = userId;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public void setName(String name){
+        this.name = name;
+    }
+
+    public String getEmail(){
+        return email;
+    }
+    public void setEmail(String email){
+        this.email = email;
+    }
+
+    public String getPassword(){
+        return password;
+    }
+    public void setPassword(String password){
+        this.password = password;
+    }
 }

53568ff726bc313ca2c4582ddb8acf20fbfa0943
zero-to-yoshino <k-yoshino@croooober.co.jp>
2022-06-06 16:57:30 +0900

    Change: HomeControllerTestの内容を公式ドキュメントと同じにした

--- test/controllers/HomeControllerTest.java
+++ test/controllers/HomeControllerTest.java
@@ -1,32 +1,24 @@
 package controllers;
 
-import org.junit.Test;
-import play.Application;
-import play.inject.guice.GuiceApplicationBuilder;
-import play.mvc.Http;
-import play.mvc.Result;
-import play.test.WithApplication;
-
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 import static play.mvc.Http.Status.OK;
-import static play.test.Helpers.GET;
-import static play.test.Helpers.route;
+import static play.test.Helpers.*;
 
-public class HomeControllerTest extends WithApplication {
+import javaguide.tests.controllers.HomeController;
 
-    @Override
-    protected Application provideApplication() {
-        return new GuiceApplicationBuilder().build();
-    }
+import org.junit.Test;
+
+import play.mvc.Result;
+import play.twirl.api.Content;
+
+public class HomeControllerTest {
 
     @Test
     public void testIndex() {
-        Http.RequestBuilder request = new Http.RequestBuilder()
-                .method(GET)
-                .uri("/");
-
-        Result result = route(app, request);
-        assertEquals(OK, result.status());
+      Result result = new HomeController().index();
+      assertEquals(OK, result.status());
+      assertEquals("text/html", result.contentType().get());
     }
-
-}
+  
+  }

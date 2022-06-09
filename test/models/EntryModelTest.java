c74cee93c3e182c8ea5af1c0d3908f05e65a3859
zero-to-yoshino <k-yoshino@croooober.co.jp>
2022-06-09 14:44:58 +0900

    Add: null値のテスト

--- test/models/EntryModelTest.java
+++ test/models/EntryModelTest.java
@@ -48,5 +48,32 @@ public class EntryModelTest {
   }
 
   @Test
-  public void 
+  public void 名前にnull値の保存を認めない() throws Exception {
+    Connection connection = database.getConnection();
+    try {
+        connection.prepareStatement("insert into entry values (10, null, 'title', 'message')").execute();
+    } catch(org.h2.jdbc.JdbcSQLException e) {       
+        assertThat(e.getMessage(), is(containsString("列 \"NAME\" にはnull値が許されていません")));
+    }
+  }
+
+  @Test
+  public void タイトルにnull値の保存を認めない() throws Exception {
+    Connection connection = database.getConnection();
+    try {
+        connection.prepareStatement("insert into entry values (10, 'name', null, 'message')").execute();
+    } catch(org.h2.jdbc.JdbcSQLException e) {       
+        assertThat(e.getMessage(), is(containsString("列 \"TITLE\" にはnull値が許されていません")));
+    }
+  }
+
+  @Test
+  public void メッセージにnull値の保存を認めない() throws Exception {
+    Connection connection = database.getConnection();
+    try {
+        connection.prepareStatement("insert into entry values (10, 'name', 'title', null)").execute();
+    } catch(org.h2.jdbc.JdbcSQLException e) { 
+        assertThat(e.getMessage(), is(containsString("列 \"MESSAGE\" にはnull値が許されていません")));
+    }
+  }
 }
